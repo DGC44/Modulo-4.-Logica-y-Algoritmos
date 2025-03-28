1eaa3c0f3960699fdf1bafa2d58b36a2b45d86c5
DianaGC <garcapdia@hotmail.com>
2025-03-27 21:56:48 -0600

    tarea 4 ajustes

--- Clase4/tarea4.js
+++ Clase4/tarea4.js
@@ -5,9 +5,12 @@ function encontrarPareja(arr) {
     let siguiente = 1;
 
     while (siguiente < arr.length) {
+        // Compara las iniciales de los nombres en los punteros
         if (arr[inicio][0] === arr[siguiente][0]) {
+            // Si coinciden, devuelve el par
             return [arr[inicio], arr[siguiente]];
         }
+        // Avanza los punteros si no coinciden
         inicio++;
         siguiente++;
     }
